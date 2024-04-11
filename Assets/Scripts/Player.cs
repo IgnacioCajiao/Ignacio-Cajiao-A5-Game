b2250625864b559cf699e101420f480a848c387f
Ignacio Cajiao <000930213@mohawkcollege.ca>
2024-04-11 18:55:52 -0400

    Add player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private bool thrust;
+    private float turnDirection;
+    private Rigidbody2D rb;
+    public float thrustSpeed = 1.0f;
+    public float turnSpeed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        thrust = Input.GetKey(KeyCode.UpArrow);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            turnDirection = 1.0f;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            turnDirection = -1.0f;
+        else turnDirection = 0.0f;
+
+    }
+    public void FixedUpdate()
+    {
+        if (thrust)
+            rb.AddForce(this.transform.up * this.thrustSpeed);
+
+        if (turnDirection != 0.0f)
+        {
+            rb.AddTorque(turnDirection * this.turnSpeed);
+        }
+            
     }
 }
